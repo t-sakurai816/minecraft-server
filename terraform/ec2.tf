c63e72c1ac58e188c68884887637ae4a63dc00f7
t-sakurai816 <t.devsaku@gmail.com>
2021-07-30 13:15:21 +0900

    CREATE: インスタンス

---
+++ terraform/ec2.tf
@@ -0,0 +1,39 @@
+#AmazonLinux2の最新AMIを取得
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*"]
+  }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_instance" "instance" {
+  ami                     = data.aws_ami.recent_amazon_linux_2.image_id # latest-AmazonLinux2
+  instance_type           = "t3a.micro"
+  vpc_security_group_ids  = [aws_security_group.minecraft.id]
+  subnet_id               = aws_subnet.public_1a.id
+  key_name                = "minecraft"
+  availability_zone       = "ap-northeast-1a"
+  disable_api_termination = false
+
+  root_block_device {
+    volume_type = "standard"
+    volume_size = "15"
+  }
+
+  tags = {
+    Name = "mincecraft-server"
+  }
+
+}
+
+# EIP
+resource "aws_eip" "instance" {
+  instance = aws_instance.instance.id
+  vpc      = true
+}
---
+++ terraform/security_group.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "minecraft" {
+  name        = "minecraft"
+  description = "for minecraft server"
+  vpc_id      = aws_vpc.minecraft.id
+
+  ingress {
+    description = "SSH from My House"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["39.111.235.214/32"]
+  }
+
+  ingress {
+    description      = "HTTP from ALL"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from ALL"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # TODO: minecraft server
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "minecraft"
+  }
+}
