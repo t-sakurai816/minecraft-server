0dc1cc14569a1d86ef1d8460892d02b6d6600d8b
t-sakurai816 <t.devsaku@gmail.com>
2021-08-20 21:08:22 +0900

    FIX: json

--- ansible.sh
+++ ansible.sh
@@ -4,7 +4,7 @@ aws ssm send-command \
  --document-name "AWS-ApplyAnsiblePlaybooks" \
  --document-version "1" \
  --targets '[{"Key":"InstanceIds","Values":["INSTANCE_ID"]}]' \
- --parameters '{"SourceType":["GitHub"],"SourceInfo":["{ \"owner\": \"t-sakurai816\", \"repository\": \"REPO_NAME\",  \"getOptions\": \"branch:main\", \"path\": \"ansible.zip\", "InstallDependencies":["True"],"PlaybookFile":["ansible/site.yml"],"ExtraVariables":["SSM=True"],"Check":["False"],"Verbose":["-v"],"TimeoutSeconds":["3600"]}' \
+ --parameters '{"SourceType":["GitHub"],"SourceInfo":["{ \"owner\": \"t-sakurai816\", \"repository\": \"REPO_NAME\",  \"getOptions\": \"branch:main\", \"path\": \"ansible.zip\" }"], "InstallDependencies":["True"],"PlaybookFile":["ansible/site.yml"],"ExtraVariables":["SSM=True"],"Check":["False"],"Verbose":["-v"],"TimeoutSeconds":["3600"]}' \
  --timeout-seconds 600 \
  --max-concurrency "50" \
  --max-errors "0" \
